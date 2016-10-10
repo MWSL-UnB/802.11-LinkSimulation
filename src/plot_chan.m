e2b7c929ab1f7a068e3a30ea3741bf2b0100d532
CalilQ <calil_queiroz@hotmail.com>
2016-10-10 15:36:13 -0300

    Improved channel plots

--- src/plot_chan.m
+++ src/plot_chan.m
@@ -1,4 +1,5 @@
 clc
+close all
 
 %% Load variables
 
@@ -7,37 +8,37 @@ load('test_results.mat');
 %% Plot time domain
 
 figure(1);
-plot((t-min(t))*1e9,abs(c)*1e3,'LineWidth',1.2);
-axis([0 1900 0 600]);
-xlabel('Tempo t [ns]');
-ylabel('|h(t)| [mV]');
+plot((t-min(t))*1e9,abs(c),'LineWidth',1.2);
+axis([0 1900 0 0.6]);
+xlabel('t [ns]');
+ylabel('|h(t)|');
 print('-dpng','figs\channel_response_time.png');
 
 %% Plot S
 
 figure(2);
-plot((t-min(t))*1e9,abs(c.^2)*1e3,'LineWidth',1.2);
-axis([0 1800 0 320]);
+plot((t-min(t))*1e9,10*log10(abs(c).^2),'LineWidth',1.2);
+axis([0 1900 -60 0]);
 xlabel('Atraso \tau [ns]');
-ylabel('|S(\tau)| [mW]');
+ylabel('|S(\tau)| [dB]');
 print('-dpng','figs\channel_response_S.png');
 
 %% Plot frequency
 
-C = fftshift(fft(c,parameters.Nfft,2));
+H = fftshift(fft(c.^2,parameters.Nfft,2));
 bw2 = c_sim.w_channel/2;
-freq = linspace(-bw2,bw2,length(R));
+freq = linspace(-bw2,bw2,length(H));
 
 figure(3);
-plot(freq,abs(C),'LineWidth',1.2);
+plot(freq,10*log10(abs(H)),'LineWidth',1.2);
 
 xlabel('Frequência normalizada f [MHz]');
-ylabel('|C(f)| [mV]');
+ylabel('|H(f)| [dB]');
 print('-dpng','figs\channel_response_freq.png');
 
 %% Plot R
 
-R = fftshift(fft(abs(c.^2),parameters.Nfft,2));
+R = fftshift(fft(abs(c).^2,parameters.Nfft,2));
 bw2 = c_sim.w_channel/2;
 freq = linspace(-bw2,bw2,length(R));
 
